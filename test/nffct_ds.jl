d660aa573170c686225edb6484908e6da10cb5df
Pascal Schröter <passscoed@gmail.com>
2022-11-07 15:57:28 +0100

    fixes get_matrix

--- test/nffct_ds.jl
+++ test/nffct_ds.jl
@@ -12,5 +12,7 @@ X = rand(d, M)
 
 X[.!dcos,:] = X[.!dcos,:] .- 0.5
 
+# set up transform ###################################################
+
 F = GroupedTransform("expcos", d, ds, [2^6, 2^4, 4], X, dcos)
 F_direct = get_matrix(F)
