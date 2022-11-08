f5ab42c9b4990d444d63727608068dd615079f52
Pascal Schröter <passscoed@gmail.com>
2022-11-08 10:33:50 +0100

    test

--- test/nffct_ds.jl
+++ test/nffct_ds.jl
@@ -2,25 +2,20 @@ using LinearAlgebra
 using GroupedTransforms
 using Test
 
-d = 4
+d = 1
 
-ds = 3
+ds = 1
 
-dcos = [true,false,true,false]
+dcos = [false]
 
-M = 1_000
+M = 2
 X = rand(d, M)
 
 X[.!dcos,:] = X[.!dcos,:] .- 0.5
 
 # set up transform ###################################################
 
-F = GroupedTransform("expcos", d, ds, [2^6, 2^4, 4], X, dcos)
-for s = F.setting
-    println("######")
-    println(s[:bandwidths])
-    println(s[:mode].datalength(s[:bandwidths]))
-end
+F = GroupedTransform("expcos", d, ds, [4], X, dcos)
 F_direct = get_matrix(F)
 
 # compute transform with NFFT ########################################
@@ -33,23 +28,23 @@ end
 
 # arithmetic tests ###################################################
 
-ghat = GroupedCoefficients(F.setting)
-for i = 1:length(F.setting)
-    u = F.setting[i][:u]
-    ghat[u] = rand(ComplexF64, size(ghat[u]))
-end
+#ghat = GroupedCoefficients(F.setting)
+#for i = 1:length(F.setting)
+#    u = F.setting[i][:u]
+#    ghat[u] = rand(ComplexF64, size(ghat[u]))
+#end
 
-fhat[1]
-fhat[1] = 1.0 + 1.0 * im
-2 * fhat
-fhat + ghat
-fhat - ghat
+#fhat[1]
+#fhat[1] = 1.0 + 1.0 * im
+#2 * fhat
+#fhat + ghat
+#fhat - ghat
 #F[[1, 2]]
-GroupedTransforms.set_data!(fhat, ghat.data)
+#GroupedTransforms.set_data!(fhat, ghat.data)
 
 ###
 
-#f = F * fhat
+f = F * fhat
 
 # compute transform without NFFT #####################################
 
