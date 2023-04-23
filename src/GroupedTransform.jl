cf77293d5f2ee1ab5494390552461a16aaf2b23d
Pascal Schröter <passscoed@gmail.com>
2023-04-23 15:51:15 +0200

    NFFCTtools

--- src/GroupedTransform.jl
+++ src/GroupedTransform.jl
@@ -59,13 +59,13 @@ struct GroupedTransform
                 error("Nodes must be between 0 and 0.5.")
             end
         elseif system == "mixed"
-            if sum(getindex.([BASES],dcos).>0)>0 
-                if (minimum(X[getindex.([BASES],dcos).>0,:]) < 0) || (maximum(X[getindex.([BASES],dcos).>0,:]) > 1)
+            if sum(getindex.([NFFCTtools.BASES],dcos).>0)>0 
+                if (minimum(X[getindex.([NFFCTtools.BASES],dcos).>0,:]) < 0) || (maximum(X[getindex.([NFFCTtools.BASES],dcos).>0,:]) > 1)
                     error("Nodes must be between 0 and 1 for cosine or Chebyshev dimensions.")
                 end
             end
-            if sum(.!(getindex.([BASES],dcos).>0))>0 
-                if (minimum(X[(.!(getindex.([BASES],dcos).>0)),:]) < -0.5) || (maximum(X[(.!(getindex.([BASES],dcos).>0)),:]) > 0.5)
+            if sum(.!(getindex.([NFFCTtools.BASES],dcos).>0))>0 
+                if (minimum(X[(.!(getindex.([NFFCTtools.BASES],dcos).>0)),:]) < -0.5) || (maximum(X[(.!(getindex.([NFFCTtools.BASES],dcos).>0)),:]) > 0.5)
                     error("Nodes must be between -0.5 and 0.5 for exponentional dimensions.")
             
                 end
