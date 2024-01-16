e4373e801765cb35ee50255ee007ed5a7c68fe6f
Pascal Schröter <passscoed@gmail.com>
2024-01-16 15:30:29 +0100

    added mixed tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using Aqua
 Aqua.test_all(GroupedTransforms, ambiguities = false)
 Aqua.test_ambiguities(GroupedTransforms)
 
-tests = ["nfct_ds", "nfct_U", "nfft_ds", "nfft_U", "cwwt_ds", "cwwt_U"]
+tests = ["nfct_ds", "nfct_U", "nfft_ds", "nfft_U", "cwwt_ds", "cwwt_U", "nfmt_ds.jl", "nfmt_U.jl"]
 
 for t in tests
     include("$(t).jl")
