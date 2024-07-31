76a2394fc658e08743ba0cb70d247d125cba81f5
Pascal Schröter <passscoed@gmail.com>
2024-07-31 14:34:28 +0200

    changed order

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Aqua
 Aqua.test_all(GroupedTransforms, ambiguities = false)
 Aqua.test_ambiguities(GroupedTransforms)
 
-tests = ["nfct_ds", "nfct_U", "nfft_ds", "nfft_U", "cwwt_ds", "cwwt_U", "nfmt_ds", "nfmt_U"]
+tests = ["cwwt_ds", "cwwt_U", "nfmt_ds", "nfmt_U", "nfct_ds", "nfct_U", "nfft_ds", "nfft_U"]
 
 for t in tests
     include("$(t).jl")
